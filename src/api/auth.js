9492053ade4d1c7616d9a3e7d9ad4518447f8c32
macbook <macbook@macbooks-MacBook-Air.local>
2023-03-29 16:27:35 +0800

    change api users

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,27 +1,9 @@
-import roles from "../constants/roles";
-import userTypes from "../constants/userTypes";
+import users from "../constants/users.json";
 
 export const login = ({ email, password }) => {
-  if (email === "counselor@gmail.com") {
-    return {
-      email,
-      userType: userTypes.CWT_USER,
-      roles: [roles.GLOBAL_MOBILE_COUNSELOR, roles.ROLE_UNMASK_CREDITCARD],
-    };
-  } else if (email === "ctm@gmail.com") {
-    return {
-      email,
-      userType: userTypes.CWT_TRAVELER,
-      roles: [roles.ROLE_CLIENT_TRAVELER_MAINTAINER],
-    };
-  } else if (email === "traveler@gmail.com") {
-    return {
-      email,
-      userType: userTypes.CWT_TRAVELER,
-      roles: [],
-    };
-  }
-  return {
-    error: "Cant login",
-  };
+  return (
+    users[email] || {
+      error: "Cant login",
+    }
+  );
 };
